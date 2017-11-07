9bf93fe646dc8cf57d15978ab72ff359ed7b39a3
Ian Hancock <iandhancock@hotmail.com>
2017-11-07 00:32:44 +0000

    Test with correct registers set - seems data not reading corrrect

--- BLHeli_S SiLabs/BLHeli_S.asm
+++ BLHeli_S SiLabs/BLHeli_S.asm
@@ -511,7 +511,7 @@ CSEG AT 1B00h				;	[Frq], [Oct,Dur]
 Eep_Pgm_Music_Notes:		DB  6eh, 22h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h	; EEPROM copy of programmed 8 note bank (16 bytes)
 Eep_Pgm_Music_Durations:	DB	0ffh, 64h, 0deh								; Duration 0 is not used (means end of tune)
 Eep_Pgm_Music_DurationsSpc:	DS	13 
-Eep_Pgm_Music_Tunes:		DB  01h, 01h, 0ffh								; EEPROM copy of programmed 40 note tune (40 bytes)
+Eep_Pgm_Music_Tunes:		DB  01h, 01h, 00h								; EEPROM copy of programmed 40 note tune (40 bytes)
 Eep_Pgm_Music_TunesSpace:	DS	37
 
 ;**** **** **** **** ****
@@ -4093,6 +4093,10 @@ PlayNote:
 	movc A, @A+DPTR					; Read Duration of Note
 	mov Temp3, A					; Duration of note in Temp3
 
+	mov	Temp3, #222 								;length of tone		
+	mov Temp4, #110		
+	mov Temp5, #2									;one ms ;frequency of tone 1=500, 2=1000, 3=1500		
+
 	jmp music						; Play the note
 
 	pop ACC							; Retrieve the [Note] and [Rest] Byte
@@ -4101,6 +4105,7 @@ PlayNote:
 	movc A, @A+DPTR					; Read Duration of [Rest]
 	mov Temp2, A					; Duration of [Rest]
 
+	mov Temp2, #100		
 	call waitTemp2ms				; Wait the rest
 
 	mov DPTR, #Eep_Pgm_Music_Tunes	; The music score - list of notes and rests to play
